8f9e348539af0ddbf84e292f02c851623a90b33b
Jagpreet-Sifive <52394430+Jagpreet-Sifive@users.noreply.github.com>
2019-07-24 10:54:53 +0530

    Update Pipo.scala

--- Update/main/scala/Pipo.scala
+++ Update/main/scala/Pipo.scala
@@ -6,8 +6,8 @@ import chisel3.util.Cat
  class Pipo extends Module{ 
  
  val io =IO(new Bundle{
-  val in = Input(UInt(4.W))
-   val out =Output(UInt(4.W))
+  val in = Input(UInt(1.W))
+   val out =Output(UInt(1.W))
  })
 
  /* all registers init. with '0' value  */
